a146355741f5dfb9e0a0d461fb7102e23090cc4e
Dan Shevlyuk <danshevlyuk@icloud.com>
2015-10-05 12:09:27 +0300

    Initial commit

---
+++ Ayes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ayes
+//
+//  Created by Dan Shevlyuk on 05/10/15.
+//  Copyright © 2015 BinaryBlitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
