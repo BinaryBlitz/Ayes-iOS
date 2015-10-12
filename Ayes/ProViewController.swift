d089bbd496629de6e387ea8b5c84439726c44729
Dan Shevlyuk <danshevlyuk@icloud.com>
2015-10-12 20:47:33 +0300

    Create ProViewController.

---
+++ Ayes/ProViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProViewController.swift
+//  Ayes
+//
+//  Created by Dan Shevlyuk on 12/10/15.
+//  Copyright © 2015 BinaryBlitz. All rights reserved.
+//
+
+import UIKit
+
+class ProViewController: UIViewController {
+
+  @IBOutlet weak var menuBarButtonItem: UIBarButtonItem!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    if let revealViewController = revealViewController() {
+      menuBarButtonItem.target = revealViewController
+      menuBarButtonItem.action = "revealToggle:"
+      view.addGestureRecognizer(revealViewController.panGestureRecognizer())
+      revealViewController.rearViewRevealWidth = SIDE_BAR_WIDTH
+    }
+  }
+}
